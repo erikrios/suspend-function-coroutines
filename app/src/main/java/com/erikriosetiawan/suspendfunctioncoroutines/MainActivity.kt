15cb679c4ba05a13f4e49bd43f0c9cf74d13296c
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-04-28 19:17:47 +0700

    Implements suspend functions with Coroutines

--- app/src/main/java/com/erikriosetiawan/suspendfunctioncoroutines/MainActivity.kt
+++ app/src/main/java/com/erikriosetiawan/suspendfunctioncoroutines/MainActivity.kt
@@ -2,11 +2,38 @@ package com.erikriosetiawan.suspendfunctioncoroutines
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.coroutines.Delay
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private val TAG = MainActivity::class.java.simpleName
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        GlobalScope.launch {
+            val networkCallAnswerOne = doNetworkCall()
+            val networkCallAnswerTwo = doNetworkCallTwo()
+
+            Log.d(TAG, networkCallAnswerOne)
+            Log.d(TAG, networkCallAnswerTwo)
+        }
+    }
+
+    private suspend fun doNetworkCall(): String {
+        delay(3000L)
+        return "This is the answer from network call one"
+    }
+
+    private suspend fun doNetworkCallTwo(): String {
+        delay(3000L)
+        return "This is the answer from network call two"
     }
 }
